95020d23df8e2228feb343e89b6aa5c55fc2a2a8
Vinojan Veerapathrathasan <vinojan02abhimanyu@gmail.com>
2022-08-01 23:34:44 +0530

    Question one is done

--- ex1.js
+++ ex1.js
@@ -1,10 +1,19 @@
-function showMessage() {
-    var message = {
-        string1: "Hello Vinojan Abhimanyu",
-        string2: "Babyyy"
-    }
-    document.getElementById('hello').innerHTML = message.string2
-    // document.getElementById('hello').innerHTML = typeof message.string1;
+// function showMessage() {
+//     var message = {
+//         string1: "Hello Vinojan Abhimanyu",
+//         string2: "Babyyy"
+//     }
+//     document.getElementById('hello').innerHTML = message.string2
+//     // document.getElementById('hello').innerHTML = message.string1.length
+//     // document.getElementById('hello').innerHTML = typeof message.string1;
 
-    document.getElementById('date').innerHTML = Date()
+//     document.getElementById('date').innerHTML = Date()
+// }
+
+function showResult() {
+    var num = prompt("Enter the number to culculate...!");
+    let result = num * num;
+
+    document.getElementById('number').innerHTML = "Entered the number is :" + num;
+    document.getElementById('result').innerHTML = "The Result is :" + result;
 }
